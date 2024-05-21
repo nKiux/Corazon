02306a5645de87d68fcdba5387ed2486f178a088
nKKai <46084374+nKiux@users.noreply.github.com>
2024-05-21 11:29:00 +0800

    v1.3.2

--- kernel_LightEngine.py
+++ kernel_LightEngine.py
@@ -13,9 +13,6 @@ import numpy as np
 from tqdm.rich import tqdm
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-from scipy.signal import find_peaks_cwt
-from scipy.signal import iirfilter
-from scipy.signal import sosfiltfilt
 
 def start(skipDMX, camera_select, mode):
     import main
@@ -52,36 +49,40 @@ def start(skipDMX, camera_select, mode):
     prog.close()
     
     if main.start(skipDMX = skipDMX, camera_select = camera_select, mode = mode) == True:
-        with open('test.txt', 'r', encoding='utf-8') as res:
-            result = [float(line.strip()) for line in res if line]
-            res.close()
-        datacount = len(np.array(result))
-        #peak = find_peaks_cwt(np.array(result), widths=np.arange(5,11))
-        #peak= find_peaks_cwt(np.array(result), widths=result)
-        x = np.arange(len(result))
-        p = np.poly1d(np.polyfit(x, result, 13))
-        pfix = p(x)[:]
-        for i in range(30):
-            pfix[i] = np.average(pfix[i:i+15])
-        for i in range(-25, 0, 1):
-            pfix[i] = np.average(pfix[i-15:i])
-        peak, _ = find_peaks(np.array(result), distance=(8.5*(datacount/200)), height=pfix)
-        print(peak)
-        
-        plt.plot(np.array(result))
-        plt.xlabel('frames')
-        plt.ylabel('Brightness')
-        plt.plot(peak, np.array(result)[peak], 'x')
-        plt.plot(x, pfix - 0.02, '-')
-        plt.show()
-        
-        finalResult = (len(peak)*6)
-        print(finalResult)
-        open('result.txt', 'w', encoding='utf-8').write(str(finalResult))
+        polft = polft_algorithm()
         cv2.destroyAllWindows()
         return True
     else:
         cv2.destroyAllWindows()
         return False
+
+def polft_algorithm():
+    with open('test.txt', 'r', encoding='utf-8') as res:
+            result = [float(line.strip()) for line in res if line]
+            res.close()
+    datacount = len(np.array(result))
+    #peak = find_peaks_cwt(np.array(result), widths=np.arange(5,11))
+    #peak= find_peaks_cwt(np.array(result), widths=result)
+    x = np.arange(len(result))
+    p = np.poly1d(np.polyfit(x, result, 13))
+    pfix = p(x)[:]
+    for i in range(30):
+        pfix[i] = np.average(pfix[i:i+15])
+    for i in range(-25, 0, 1):
+        pfix[i] = np.average(pfix[i-15:i])
+    peak, _ = find_peaks(np.array(result), distance=(8.5*(datacount/200)), height=pfix)
+    print(peak)
+    
+    plt.plot(np.array(result))
+    plt.xlabel('frames')
+    plt.ylabel('Brightness')
+    plt.plot(peak, np.array(result)[peak], 'x')
+    plt.plot(x, pfix - 0.02, '-')
+    plt.show()
+    
+    finalResult = (len(peak)*6)
+    print(finalResult)
+    open('result.txt', 'w', encoding='utf-8').write(str(finalResult))
+    return finalResult
 
 #start(False, 0 , 0)
--- main.py
+++ main.py
@@ -24,38 +24,22 @@ from tqdm.rich import tqdm
 
 def start(skipDMX, camera_select, mode):
     
-    '''if mode == 0:
-        print('mode is Fast')
-        D_speed = "Fast"
-    elif mode == 1:
-        print('mode is Normal')
-        D_speed = "Normal"
-    else:
-        print('mode is Slow')
-        D_speed = "Slow"'''
     start_t = int(time.time())
     cam = cv2.VideoCapture(camera_select)
     cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
     counting = 0
-    chk_count = 0
-    mx = 0
-    mn = 255
     FDetect = False
     if_reset = False
     reset = False
     passed = False
-    beats = 0
     bpm = 0
     brit = -4
     bright_fail_count = 0
     britFailContrl = False
     autobrit = False
-    #v0.6.6
-    #img = np.empty((300, 300, 3), np.uint8)
-    #os.system('WCConfig.exe') #msvc120
     global fatalError
     fatalError = 0
-
+    aeset = False
     while True:
         time_now = time.time_ns()
         
@@ -67,9 +51,6 @@ def start(skipDMX, camera_select, mode):
             print('Camera Start Failed!')
             return False
 
-        #window = win32gui.FindWindow(None, 'Camera_Capture')
-        #win_rectPast = win32gui.GetWindowRect(window)
-        
         if cv2.waitKey(1) == ord('q'):
             print('Exiting...')
             cv2.destroyAllWindows()
@@ -77,7 +58,7 @@ def start(skipDMX, camera_select, mode):
         
         frm = cv2.resize(frm,(600,420))
         avgB, avgG, avgR, avgA = cv2.mean(frm)
-        #v0.6.6
+        
         gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
         bright = cv2.mean(gray)[0]
         if str(bright)[1] == '.':
@@ -90,34 +71,37 @@ def start(skipDMX, camera_select, mode):
         # Finger detection
         if avgR > 70 and avgR > (avgB + avgG) and counting <= 10:
             if counting > 6:
-                britFailContrl = False
                 if brit <= -3:
                     brit = -4 + bright_fail_count
                     cam.set(cv2.CAP_PROP_EXPOSURE, brit)
                 elif brit > -3:
-                    cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+                    if aeset == False:
+                        cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
                     autobrit = True
+                    aeset = True
                 cam.set(cv2.CAP_PROP_BRIGHTNESS, 100)
             FDetect = True
             counting += 0.2
-        elif avgR > 70 and avgR > (avgB + avgG) and counting > 10:
+        elif avgR > 70 and avgR > (avgB + avgG) and counting > 10 and aeset == False:
             if brit > -3:
                 cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+                aeset = True
             else:
                 cam.set(cv2.CAP_PROP_EXPOSURE, brit)
                 cam.set(cv2.CAP_PROP_BRIGHTNESS, 100)
+                aeset = True
+        elif avgR > 70 and avgR > (avgB + avgG) and counting > 10 and aeset == True:
+            pass
         else:
             if britFailContrl == False:
                 bright_fail_count += 1
             britFailContrl = True
-            cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
-            cam.set(cv2.CAP_PROP_BRIGHTNESS, 100)
+            if aeset == False:
+                cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+                cam.set(cv2.CAP_PROP_BRIGHTNESS, 100)
+                aeset = True
             FDetect = False
-            mx = 0
-            mn = 255
-            chk_count = 30
             passed = False
-            beats = 0
             bpm = 0
             if counting >= 0:
                 counting -= 2
