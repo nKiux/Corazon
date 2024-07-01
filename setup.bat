ff8d7ec530f87f19c11a6d6185b4f41d778ce15e
KnowScratcher <yianlee2008@gmail.com>
2024-07-01 15:00:00 +0800

    update setup

--- setup.bat
+++ setup.bat
@@ -4,4 +4,13 @@ PAUSE
 pip install PyQt5 scipy opencv-python
 echo:
 echo setup done!
-PAUSE
+choice /c:YN /m:"would you like to launch the app now?"
+echo %ERRORLEVEL%
+goto setup%errorlevel%
+
+:setup1
+start HRMonitor2.0.exe
+
+
+:setup2
+
